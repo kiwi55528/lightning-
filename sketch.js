8a5cfd02e68535cdcdb020aa0fad55cce7bfcd5c
kiwi55528 <67670177+kiwi55528@users.noreply.github.com>
2020-08-11 20:23:39 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball,database,position,pos;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    database=firebase.database()
+    pos=database.ref("ball/position")
+    pos.on("value",readPosition)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+ //   ball.x = ball.x + x;
+ //   ball.y = ball.y + y;
+ database.ref("ball/position").set({
+X:position.X+x,
+Y:position.Y+y
+
+
+ })
+}
+function readPosition(data){
+position=data.val()
+ball.x=position.X
+ball.y=position.Y
+
+
+
+
+}
